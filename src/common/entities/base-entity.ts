bbe6d35787b64e50215c9ee08b71705224b64031
MyITjournal <adesitjourney@gmail.com>
2025-12-07 11:33:10 +0100

    fix: add type to base-entity 'deleted_at'

--- src/common/entities/base-entity.ts
+++ src/common/entities/base-entity.ts
@@ -7,17 +7,17 @@ import {
 
 export abstract class BaseEntity {
   @PrimaryGeneratedColumn('uuid')
-  id: string;
+  id!: string;
 
   @CreateDateColumn({ name: 'created_at' })
-  created_at: Date;
+  created_at!: Date;
 
   @UpdateDateColumn({ name: 'updated_at' })
-  updated_at: Date;
+  updated_at!: Date;
 
   @Column({ name: 'is_deleted', default: false })
-  is_deleted: boolean;
+  is_deleted!: boolean;
 
-  @Column({ name: 'deleted_at', nullable: true })
-  deleted_at: Date | null;
+  @Column({ name: 'deleted_at', type: 'timestamp', nullable: true })
+  deleted_at!: Date | null;
 }
