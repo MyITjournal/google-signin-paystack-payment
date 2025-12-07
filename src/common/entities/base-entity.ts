2fd5b66e9c7e3e37d3b15c0ce11365e658c3cb96
MyITjournal <adesitjourney@gmail.com>
2025-12-07 02:00:05 +0100

    feat: added base entity file

---
+++ src/common/entities/base-entity.ts
@@ -0,0 +1,23 @@
+import {
+  CreateDateColumn,
+  UpdateDateColumn,
+  PrimaryGeneratedColumn,
+  Column,
+} from 'typeorm';
+
+export abstract class BaseEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @CreateDateColumn({ name: 'created_at' })
+  created_at: Date;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updated_at: Date;
+
+  @Column({ name: 'is_deleted', default: false })
+  is_deleted: boolean;
+
+  @Column({ name: 'deleted_at', nullable: true })
+  deleted_at: Date | null;
+}
